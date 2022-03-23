0a7ed752ebe4c412418a77502110460166a3114e
Andrzej Gruszczyk <andrzej.gruszczyk@gmail.com>
2022-03-23 21:28:38 +0100

    Initial version

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -4,14 +4,31 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.SecureRandom;
+import java.util.UUID;
+
 @Slf4j
 @RestController
 public class DemoController {
 
-    @GetMapping("random-uuid")
+    private final SecureRandom random;
+
+    public DemoController() {
+        random = new SecureRandom("seed".getBytes());
+    }
+
+    @GetMapping("weather")
+    public String getRandomWeather() {
+        final var nextInt = random.nextInt(100);
+        final var msg = String.format("The weather is %d%% sunny today", nextInt);
+        log.info(msg);
+        return msg;
+    }
+
+    @GetMapping("uuid")
     public String getRandomUuid() {
-        var result = "aaa";
-        log.info("Received REST request, returning {}", result);
+        var result = UUID.randomUUID().toString();
+        log.info("Returning UUID {}", result);
         return result;
     }
 }
